ccb82cc6f8bef2e6b79923121a7986372f596732
khoand <khnguyendinh@gmail.com>
2017-04-26 11:25:27 +0700

    hoan thanh giao dien

---
+++ app/src/main/java/app/lampstudio/com/quanlychitieu/Manager_acc.java
@@ -0,0 +1,15 @@
+package app.lampstudio.com.quanlychitieu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Manager_acc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_acc);
+
+    }
+
+}
